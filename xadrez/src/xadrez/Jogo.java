83fa1e21cbf7c9588225587d3217cd82a37bef21
Bianca Ribeiro <90845861+bumblebib@users.noreply.github.com>
2024-08-31 11:00:21 -0300

    Iniciada a classe Jogo

--- xadrez/src/xadrez/Jogo.java
+++ xadrez/src/xadrez/Jogo.java
@@ -1,29 +1,59 @@
 /*
-A cada jogada solicitada por um jogador, o Jogo é a classe que dispara a jogada checando
-se ela é válida, atualizando o tabuleiro, a situação, o histórico de jogadas, a tela, a situação
-do jogo, das peças, etc, usando sempre as chamadas de métodos das outras classes que
-forem apropriados.
-Todas as jogadas efetivamente realizadas devem ser registradas em um histórico de
-jogadas que pode ser solicitado pelo Gerenciador para armazenamento em arquivo ou
-visualização das jogadas.
-A classe Jogo deve ter ao menos os métodos:
-
-boolean jogadaValida(linhaO, colunaO, linhaD, colunaD): verifica se a jogada é
-válida ou não
-
-void realizarJogada(linhaO, colunaO, linhaD, colunaD): se a jogada é válida,
-atualiza o posicionamento das peças no tabuleiro, a situação do jogo, etc, e mostra
-na tela as informações apropriadas.
-
-String registroJogo(): deve retornar uma string com todos os dados relevantes do
-jogo para que ele possa ser retomado posteriormente (ver arquivo do Gerenciador).
+        Não completa!!
+        O jogo cria até 2 jogadores
+        O jogo cria um tabuleiro
+        O jogo cria uma peça
+        O jogo cria uma ou mais jogadaas e verifica estas jogadas
 */
+
 package xadrez;
 
-/**
- *
- * 
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Jogo {
+    private Tabuleiro tabuleiro;
+    private Jogador jogadores[] = new Jogador[2];
+    private ArrayList<Jogada> jogada;
+    private Peca pecas[] = new Peca[32];
+    private int estado; // inicio 1, xeque 2, xeque-mate 3
+    private int turnoJogador; // qual jogador é a vez branco(1) ou preto(2)
+    
+    public Jogo(){
+        this.jogada = new ArrayList<Jogada>();
+        setEstado(1);
+        setTurnoJogador(1); // não sei quem que começa primeiro
+        
+        // pegando informações do jogador 1 
+        System.out.println("Jogador 1, por favor informe seu nome e a cor das suas peças");
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Nome: ");
+        String nome1 = leitor.nextLine();
+        System.out.print("Cor: ");
+        String cor1 = leitor.nextLine();
+        this.jogadores[0] = new Jogador(nome1, cor1);
+        
+        // pegando informações do jogador 2
+        System.out.println("Jogador 1, por favor informe seu nome e a cor das suas peças");
+        System.out.print("Nome: ");
+        String nome2 = leitor.nextLine();
+        System.out.print("Cor: ");
+        String cor2 = leitor.nextLine();
+        this.jogadores[0] = new Jogador(nome2, cor2);
+    }
+    
+    public void setEstado(int estado){
+        this.estado = estado;
+    }
+    
+    public void setTurnoJogador(int jogador){
+        this.turnoJogador = jogador;
+    }
+    
+    public boolean jogadaValida(int linhaO, char colunaO, int linhaD,char colunaD){
+        return this.jogadaValida(linhaO, colunaO, linhaD, colunaD);
+    }
+    
+    
     
 }
